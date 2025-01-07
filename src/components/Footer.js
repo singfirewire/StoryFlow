d75321f47860d795cb6e486777f2134369ed16cf
singfirewire <84568649+singfirewire@users.noreply.github.com>
2025-01-08 06:24:39 +0700

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 text-center text-gray-400 text-xs">
+      <div>StoryFlow</div>
+      <div>Version 6</div>
+      <div>Developed by 1O1</div>
+    </div>
+  );
+};
+
+export default Footer;
